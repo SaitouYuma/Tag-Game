25d18d48e02c5ca7326d84b46c5fda3d5d013dd4
yuma12sa <yuma12sa@gmail.com>
2026-03-29 18:34:46 +0900

    [update]CameraRotateのスクリプトでカメラ移動するように

--- Assets/scripts/CameraRotate1P.cs
+++ Assets/scripts/CameraRotate1P.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraRotate1P : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject _player1P;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _minVerticalAngle = -30f; 
+    [SerializeField] private float _maxVerticalAngle = 60f;  
+
+    private Transform _Tr;
+    private InputAction _cameraAction;
+    private float _verticalAngle = 0f;
+
+    private void Start()
     {
-        
+        _Tr = transform;
+        _cameraAction = InputSystem.actions.FindAction("CameraMove");
+        if (_cameraAction == null) Debug.LogError("CameraMoveƒAƒNƒVƒ‡ƒ“‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 input = _cameraAction.ReadValue<Vector2>();
+
+        _Tr.RotateAround(_player1P.transform.position, Vector3.up, input.x * _speed * Time.deltaTime);
+
+        float newAngle = _verticalAngle - input.y * _speed * Time.deltaTime;
+        newAngle = Mathf.Clamp(newAngle, _minVerticalAngle, _maxVerticalAngle);
+        float delta = newAngle - _verticalAngle;
+        _verticalAngle = newAngle;
+
+        _Tr.RotateAround(_player1P.transform.position, _Tr.right, delta);
     }
 }
